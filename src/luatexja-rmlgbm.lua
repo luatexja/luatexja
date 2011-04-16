686f43e3a37de596f072ba951b69214f71d1c1cc
Kazuki Maeda <kmaeda@users.sourceforge.jp>
2011-04-16 21:56:37 +0900

    Optimized the mk-rmlgbm-data process for size and ltj.mk_rml() for speed.

--- src/luatexja-rmlgbm.lua
+++ src/luatexja-rmlgbm.lua
@@ -2,54 +2,77 @@ function ltj.mk_rml(name, size, id)
    ltj.rmlgbm_data = ltj.rmlgbm_data or require('luatexja-rmlgbm-data')
 
    local specification = fonts.define.analyze(name,size)
-   local method = specification.method
-   specification = fonts.define.specify[method](specification)
+   specification = fonts.define.specify[':'](specification)
    local features = specification.features.normal
 
-   -- below are taken from otfl-font-otf.lua (luaotfload v1.24)
+   local fontdata = {}
+   local cachedata = {}
+   for k, v in pairs(ltj.rmlgbm_data) do
+      fontdata[k] = v
+      cachedata[k] = v
+   end
 
-   -- from fonts.otf.otf_to_tfm()
-   local tfmdata
-   ltj.rmlgbm_data.shared = ltj.rmlgbm_data.shared or {
-      featuredata = { },
-      anchorhash  = { },
-      initialized = false,
-   }
-   tfmdata = fonts.otf.copy_to_tfm(ltj.rmlgbm_data)
-   tfmdata.unique = tfmdata.unique or { }
-   tfmdata.shared = tfmdata.shared or { } -- combine
-   local shared = tfmdata.shared
-   shared.otfdata = ltj.rmlgbm_data
-   shared.features = features -- default
-   shared.dynamics = { }
-   shared.processes = { }
-   shared.set_dynamics = fonts.otf.set_dynamics -- fast access and makes other modules independent
-   -- this will be done later anyway, but it's convenient to have
-   -- them already for fast access
-   tfmdata.luatex = ltj.rmlgbm_data.luatex
-   tfmdata.indices = ltj.rmlgbm_data.luatex.indices
-   tfmdata.unicodes = ltj.rmlgbm_data.luatex.unicodes
-   tfmdata.marks = ltj.rmlgbm_data.luatex.marks
-   tfmdata.originals = ltj.rmlgbm_data.luatex.originals
-   tfmdata.changed = { }
-   tfmdata.has_italic = ltj.rmlgbm_data.metadata.has_italic
-   if not tfmdata.language then tfmdata.language = 'dflt' end
-   if not tfmdata.script   then tfmdata.script   = 'dflt' end
-   shared.processes, shared.features = fonts.otf.set_features(tfmdata,fonts.define.check(features,fonts.otf.features.default))
+   fontdata.unicodes = nil
+   fontdata.shared = nil
+   cachedata.shared = {}
+   local shared = cachedata.shared
+   for k, v in pairs(ltj.rmlgbm_data.shared) do
+      shared[k] = v
+   end
+
+   shared.set_dynamics = fonts.otf.set_dynamics 
+   shared.processes, shared.features = fonts.otf.set_features(cachedata,fonts.define.check(features,fonts.otf.features.default))
    
-   -- from fonts.otf.read_from_open_type()
-   tfmdata = fonts.tfm.scale(tfmdata, size)
+   -- characters & scaling
+   local characters = {}
+   local orig_chars = ltj.rmlgbm_data.characters 
+   if size < 0 then size = -size * 655.36 end
+   local scale = size / 655360
+   local size_cache = {}
+   for k, v in pairs(orig_chars) do
+      characters[k] = {}
+      characters[k].index = v.index
+      characters[k].width = v.width * scale
+      characters[k].tounicode = v.tounicode
+   end
+   fontdata.characters = characters
+   cachedata.characters = characters
+
+   local parameters = {}
+   for k, v in pairs(ltj.rmlgbm_data.parameters) do
+      parameters[k] = v * scale
+   end
+   fontdata.parameters = parameters
+   cachedata.parameters = parameters
+
+   fontdata.ascender = fontdata.ascender * scale
+   cachedata.ascender = fontdata.ascender
+   fontdata.descender = fontdata.descender * scale
+   cachedata.descender = fontdata.descender
+   fontdata.factor = fontdata.factor * scale
+   cachedata.factor = fontdata.factor
+   fontdata.hfactor = fontdata.hfactor * scale
+   cachedata.hfactor = fontdata.hfactor
+   fontdata.vfactor = fontdata.vfactor * scale   
+   cachedata.vfactor = fontdata.vfactor
+   fontdata.size = size
+   cachedata.size = size
 
    -- no embedding
-   tfmdata.name = specification.name .. size
-   tfmdata.fullname = specification.name
-   tfmdata.psname = specification.name
-   tfmdata.embedding='no'; 
-   tfmdata.cache='no'
+   fontdata.name = specification.name .. size
+   cachedata.name = fontdata.name
+   fontdata.fullname = specification.name
+   cachedata.fullname = fontdata.fullname
+   fontdata.psname = specification.name
+   cachedata.psname = fontdata.psname
+   fontdata.embedding = 'no'; 
+   cachedata.embedding = fontdata.embedding
+   fontdata.cache = 'no'
+   cachedata.cache = fontdata.cache
 
-   fonts.ids[id] = tfmdata
+   fonts.ids[id] = cachedata
 
-   return tfmdata
+   return fontdata
 end
 
 local dr_orig = fonts.define.read
