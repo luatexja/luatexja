5d7ff378dee61207366053efefaa4e95fe2b4942
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2011-09-23 13:10:22 +0900

    Noted the clash between kerning from font and that from JFM.

---
+++ src/jfm-prop.lua
@@ -0,0 +1,9 @@
+luatexja.jfont.define_jfm {
+   dir = 'yoko',
+   zw = 1.0, zh = 1.0,
+
+   [0] = {
+      align = 'left', left = 0.0, down = 0.0,
+      width = 'prop', height = 0.88, depth = 0.12, italic=0.0,
+   }
+}
