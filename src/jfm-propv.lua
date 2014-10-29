e4581bb70356459eac53f47f40ff9eb4e0fb2a38
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2014-10-29 17:17:59 +0900

    jfm-propv.lua: a metric for vertical proportional Japanese fonts

---
+++ src/jfm-propv.lua
@@ -0,0 +1,9 @@
+luatexja.jfont.define_jfm {
+   dir = 'tate',
+   zw = 1.0, zh = 1.0,
+
+   [0] = {
+      align = 'left', left = 0.0, down = 0.0,
+      width = 'prop', height = 0.50, depth = 0.50, italic=0.0,
+   }
+}
