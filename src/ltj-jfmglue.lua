luatexja/luatexja

commit 0526b27bf88ed7f2adedef267f4d9226f6f5a692

get rid of attr_orig_char

src/ltj-jfmglue.lua
@@ -90,8 +90,8 @@ local tex_dir
 local attr_ablshift
 local set_np_xspc_jachar
 
-local attr_dir = luatexbase.attributes['ltj@dir']
 local attr_icflag = luatexbase.attributes['ltj@icflag']
+local ltjs_orig_char_table = ltjs.orig_char_table
 
 local function get_attr_icflag(p)
    return (has_attr(p, attr_icflag) or 0)%PROCESSED_BEGIN_FLAG
@@ -169,8 +169,8 @@ local head -- the head of current list
 local Np, Nq, Bp
 local widow_Bp, widow_Np -- \jcharwidowpenalty 挿入位置管理用
 
-local ihb_flag -- JFM グルー挿入抑止用 flag
-               -- on: \inhibitglue 指定時，hlist の周囲
+local non_ihb_flag -- JFM グルー挿入抑止用 flag
+-- false: \inhibitglue 指定時 true: それ以外
 
 -------------------- hlist 内の文字の検索
 
@@ -250,7 +250,7 @@ function check_box_high(Nx, box_ptr, box_end)
       if first_char then
          if getid(first_char)==id_glyph then
 	    if getfield(first_char, 'lang') == lang_ja then
-	       set_np_xspc_jachar(Nx, first_char)
+	       set_np_xspc_jachar_hbox(Nx, first_char)
 	    else
 	       set_np_xspc_alchar(Nx, getchar(first_char),first_char, 1)
 	    end
@@ -319,11 +319,11 @@ local min, max = math.min, math.max
 local function calc_np_aux_glyph_common(lp)
    Np.nuc = lp
    Np.id = npi
-   if getfield(lp, 'lang') == lang_ja then
+   if ltjs_orig_char_table[lp] then
       Np.id = id_jglyph
       set_np_xspc_jachar(Np, lp)
       local npi, npf
-      lp, head, npi, npf = capsule_glyph(lp, Np.met, Np.class, head, tex_dir)
+      lp, head, npi, npf = capsule_glyph(lp, Np.met, Np.class, head, tex_dir, lp)
       Np.first = (Np.first~=Np.nuc) and Np.first or npf or npi
       Np.nuc = npi
       return true, check_next_ickern(lp);
@@ -344,7 +344,7 @@ local function calc_np_aux_glyph_common(lp)
 	    lp=lx; break
 	 else
 	    local lid = getid(lx)
-	    if lid==id_glyph and getfield(lx, 'lang') ~= lang_ja then
+	    if lid==id_glyph and not ltjs_orig_char_table[lx] then
 	       -- 欧文文字
 	       last_glyph = lx; set_attr(lx, attr_icflag, PROCESSED); Np.last = lx
 	       y_adjust = has_attr(lx,attr_ablshift) or 0
@@ -443,7 +443,7 @@ local calc_np_auxtable = {
       if lps==sid_user then
 	 if getfield(lp, 'user_id')==luatexja.userid_table.IHB then
 	    local lq = node_next(lp);
-	    head = node_remove(head, lp); node_free(lp); ihb_flag = true
+	    head = node_remove(head, lp); node_free(lp); non_ihb_flag = false
 	    return false, lq;
 	 else
 	    set_attr(lp, attr_icflag, PROCESSED)
@@ -516,7 +516,7 @@ calc_np_auxtable[id_adjust] = calc_np_auxtable.skip
 function calc_np(lp, last)
    local k
    -- We assume lp = node_next(Np.last)
-   Np, Nq, ihb_flag = Nq, Np, nil
+   Np, Nq, non_ihb_flag = Nq, Np, true
    -- We clear `predefined' entries of Np before pairs() loop,
    -- because using only pairs() loop is slower.
    Np.post, Np.pre, Np.xspc = nil, nil, nil
@@ -561,13 +561,12 @@ do
 
 -- 和文文字のデータを取得
    local attr_jchar_class = luatexbase.attributes['ltj@charclass']
-   local attr_orig_char = luatexbase.attributes['ltj@origchar']
    local attr_autospc = luatexbase.attributes['ltj@autospc']
    local attr_autoxspc = luatexbase.attributes['ltj@autoxspc']
    function set_np_xspc_jachar_yoko(Nx, x)
       local m = ltjf_font_metric_table[getfont(x)]
-      local cls, c = slow_find_char_class(has_attr(x, attr_orig_char), m, getchar(x))
-      Nx.met, Nx.char = m, c; Nx.class = cls;
+      local cls, c = slow_find_char_class(ltjs_orig_char_table[x], m, getchar(x))
+      Nx.met = m; Nx.class = cls
       if cls~=0 then set_attr(x, attr_jchar_class, cls) end
       Nx.pre  = table_current_stack[PRE + c]  or 0
       Nx.post = table_current_stack[POST + c] or 0
@@ -578,12 +577,22 @@ do
    function set_np_xspc_jachar_tate(Nx, x)
       local m = ltjf_font_metric_table[getfont(x)]
       local cls, c
-      local c1, c2 = getchar(x), has_attr(x, attr_orig_char)
-      c = has_attr(x, attr_dir) or c1 or c2
+      local c1, c2 = getchar(x), ltjs_orig_char_table[x][1]
+      c = ltjs_orig_char_table[x][2] or c1 or c2
       cls = ltjf_find_char_class(c, m)
       if cls==0 then cls = slow_find_char_class(c2, m, c1) end
-      Nx.met, Nx.char = m, c; Nx.class = cls;
+      Nx.met = m; Nx.class = cls;
       if cls~=0 then set_attr(x, attr_jchar_class, cls) end
+      Nx.pre  = table_current_stack[PRE + c]  or 0
+      Nx.post = table_current_stack[POST + c] or 0
+      Nx.xspc = table_current_stack[XSP  + c] or 3
+      Nx.kcat = table_current_stack[KCAT + c] or 0
+      Nx.auto_kspc, Nx.auto_xspc = (has_attr(x, attr_autospc)==1), (has_attr(x, attr_autoxspc)==1)
+   end
+   local function set_np_xspc_jachar_hbox(Nx, x)
+      local m = ltjf_font_metric_table[getfont(x)]
+      local c = getchar(x)
+      Nx.met = m; Nx.class = has_attr(x, attr_jchar_class) or 0;
       Nx.pre  = table_current_stack[PRE + c]  or 0
       Nx.post = table_current_stack[POST + c] or 0
       Nx.xspc = table_current_stack[XSP  + c] or 3
@@ -604,9 +613,8 @@ do
 	 Nx.pre  = table_current_stack[PRE + c]  or 0
 	 Nx.post = table_current_stack[POST + c] or 0
 	 Nx.xspc = table_current_stack[XSP  + c] or 3
-	 Nx.char = 'jcharbdd'
       else
-	 Nx.pre, Nx.post, Nx.char = 0, 0, -1
+	 Nx.pre, Nx.post = 0, 0
          Nx.xspc = table_current_stack[XSP - 1] or 3
       end
       Nx.met = nil
@@ -621,7 +629,7 @@ do
       if s then
 	 if getid(s)==id_glyph then
 	    if getfield(s, 'lang') == lang_ja then
-	       set_np_xspc_jachar(Nx, s)
+	       set_np_xspc_jachar_hbox(Nx, s)
 	    else
 	       set_np_xspc_alchar(Nx, getchar(s), s, 2)
 	    end
@@ -824,22 +832,19 @@ do
 end
 
 local function calc_ja_ja_glue()
-   if  ihb_flag then return nil
+   local qm, pm = Nq.met, Np.met
+   if (qm.char_type==pm.char_type) and (qm.var==pm.var) then
+      return new_jfm_glue(qm, Nq.class, Np.class)
    else
-      local qm, pm = Nq.met, Np.met
-      if (qm.char_type==pm.char_type) and (qm.var==pm.var) then
-         return new_jfm_glue(qm, Nq.class, Np.class)
-      else
-         local npn, nqn = Np.nuc, Nq.nuc
-         local gb, db = new_jfm_glue(qm, Nq.class,
-				     slow_find_char_class(has_attr(npn, attr_orig_char),
-							  qm, getchar(npn)))
-         local ga, da = new_jfm_glue(pm,
-				     slow_find_char_class(has_attr(nqn, attr_orig_char),
-							  pm, getchar(nqn)),
-                               Np.class)
-         return calc_ja_ja_aux(gb, ga, db, da);
-      end
+      local npn, nqn = Np.nuc, Nq.nuc
+      local gb, db = new_jfm_glue(qm, Nq.class,
+				  slow_find_char_class(ltjs_orig_char_table[npn],
+						       qm, getchar(npn)))
+      local ga, da = new_jfm_glue(pm,
+				  slow_find_char_class(ltjs_orig_char_table[nqn],
+						       pm, getchar(nqn)),
+				  Np.class)
+      return calc_ja_ja_aux(gb, ga, db, da);
    end
 end
 
@@ -882,36 +887,30 @@ end
 -------------------- 隣接した「塊」間の処理
 
 local function get_OA_skip()
-   if not ihb_flag then
-      local pm = Np.met
-      return new_jfm_glue(pm,
-        fast_find_char_class(((Nq.id == id_math and -1) or (type(Nq.char)=='string' and Nq.char or 'jcharbdd')), pm), Np.class)
-   else return nil
-   end
+   local pm = Np.met
+   return new_jfm_glue(pm,
+		       fast_find_char_class((Nq.id == id_math and -1 or 'jcharbdd'), pm), Np.class)
 end
 local function get_OB_skip()
-   if not ihb_flag then
-      local qm = Nq.met
-      return new_jfm_glue(qm, Nq.class,
-        fast_find_char_class(((Np.id == id_math and -1) or'jcharbdd'), qm))
-   else return nil
-   end
+   local qm = Nq.met
+   return new_jfm_glue(qm, Nq.class,
+		       fast_find_char_class((Np.id == id_math and -1 or'jcharbdd'), qm))
 end
 
 -- (anything) .. jachar
 local function handle_np_jachar(mode)
    local qid = Nq.id
    if qid==id_jglyph or ((qid==id_pbox or qid==id_pbox_w) and Nq.met) then
-      local g = calc_ja_ja_glue() or get_kanjiskip() -- M->K
+      local g = non_ihb_flag and calc_ja_ja_glue() or get_kanjiskip() -- M->K
       handle_penalty_normal(Nq.post, Np.pre, g); real_insert(g)
    elseif Nq.met then  -- qid==id_hlist
-      local g = get_OA_skip() or get_kanjiskip() -- O_A->K
+      local g = non_ihb_flag and get_OA_skip() or get_kanjiskip() -- O_A->K
       handle_penalty_normal(0, Np.pre, g); real_insert(g)
    elseif Nq.pre then
-      local g = get_OA_skip() or get_xkanjiskip(Np) -- O_A->X
+      local g = non_ihb_flag and get_OA_skip() or get_xkanjiskip(Np) -- O_A->X
       handle_penalty_normal((qid==id_hlist and 0 or Nq.post), Np.pre, g); real_insert(g)
    else
-      local g = get_OA_skip() -- O_A
+      local g = non_ihb_flag and get_OA_skip() -- O_A
       if qid==id_glue then handle_penalty_normal(0, Np.pre, g)
       elseif qid==id_kern then handle_penalty_suppress(0, Np.pre, g)
       else handle_penalty_always(0, Np.pre, g)
@@ -927,10 +926,10 @@ end
 -- jachar .. (anything)
 local function handle_nq_jachar()
     if Np.pre then
-      local g = get_OB_skip() or get_xkanjiskip(Nq) -- O_B->X
+      local g = non_ihb_flag and get_OB_skip() or get_xkanjiskip(Nq) -- O_B->X
       handle_penalty_normal(Nq.post, (Np.id==id_hlist and 0 or Np.pre), g); real_insert(g)
    else
-      local g = get_OB_skip() -- O_B
+      local g =non_ihb_flag and  get_OB_skip() -- O_B
       if Np.id==id_glue then handle_penalty_normal(Nq.post, 0, g)
       elseif Np.id==id_kern then handle_penalty_suppress(Nq.post, 0, g)
       else handle_penalty_always(Nq.post, 0, g)
@@ -943,7 +942,7 @@ end
 local function handle_np_ja_hlist()
    local qid = Nq.id
    if qid==id_jglyph or ((qid==id_pbox or Nq.id == id_pbox_w) and Nq.met) then
-      local g = get_OB_skip() or get_kanjiskip() -- O_B->K
+      local g = non_ihb_flag and get_OB_skip() or get_kanjiskip() -- O_B->K
       handle_penalty_normal(Nq.post, 0, g); real_insert(g)
    elseif Nq.met then  -- Nq.id==id_hlist
       local g = get_kanjiskip() -- K
@@ -1015,7 +1014,7 @@ end
 local function handle_list_head(par_indented)
    local npi, pm = Np.id, Np.met
    if npi ==  id_jglyph or (npi==id_pbox and pm) then
-      if not ihb_flag then
+      if non_ihb_flag then
 	 local g = new_jfm_glue(pm, fast_find_char_class(par_indented, pm), Np.class)
 	 if g then
 	    set_attr(g, attr_icflag, BOXBDD)

src/addons/luatexja-fontspec-24.sty
@@ -359,7 +359,12 @@
 }
 
 %%% CJKShape=JIS2004
-\__fontspec_define_feature_option:nnnnn{CJKShape}{JIS2004}{20}{5}{+jp04}
+\__fontspec_define_feature_option:nnnnn{CJKShape}{JIS2004}{20}{100}{+jp04}
+%%% CharacterWidth={VerticalAlternateProportional, VerticalAlternateHalf}
+\__fontspec_define_feature_option:nnnnn{CharacterWidth}{VerticalAlternateProportional}{22}{100}{+vpal}
+\__fontspec_define_feature_option:nnnnn{CharacterWidth}{VerticalAlternateHalf}{22}{101}{+vhal}
+\__fontspec_define_feature_option:nnnnn{Kerning}{Vertical}       {}{}{+vkrn}
+
 
 \ExplSyntaxOff
 %%%%%%%% Now we completely ignore kanjifont definition file.

src/ltj-jfont.lua
@@ -829,20 +829,22 @@ luatexbase.add_to_callback(
       local vadv = {}; fmtable.v_advance = vadv
       local ft = font_getfont(fnum)
       local subtables = {}
-      for feat_name,v in pairs(ft.specification.features.normal) do
-      	 if v then
-      	    for _,i in pairs(ft.resources.sequences) do
-      	       if i.order[1]== feat_name and i.type == 'gpos_single' then
-		  for _,st in pairs(i.subtables) do
-		     subtables[st] = true
+      if ft.specification then
+	 for feat_name,v in pairs(ft.specification.features.normal) do
+	    if v then
+	       for _,i in pairs(ft.resources.sequences) do
+		  if i.order[1]== feat_name and i.type == 'gpos_single' then
+		     for _,st in pairs(i.subtables) do
+			subtables[st] = true
+		     end
 		  end
 	       end
-      	    end
-      	 end
-      end
-      acc_feature(vadv, subtables, ft)
-      for i,v in pairs(vadv) do
-	 vadv[i]=vadv[i]/ft.units_per_em*fmtable.size
+	    end
+	 end
+	 acc_feature(vadv, subtables, ft)
+	 for i,v in pairs(vadv) do
+	    vadv[i]=vadv[i]/ft.units_per_em*fmtable.size
+	 end
       end
       return fmtable 
    end, 1, 'ltj.v_advance'

src/ltj-pretreat.lua
@@ -43,7 +43,7 @@ local attr_curtfnt = luatexbase.attributes['ltj@curtfnt']
 local attr_icflag = luatexbase.attributes['ltj@icflag']
 
 local is_ucs_in_japanese_char = ltjc.is_ucs_in_japanese_char_direct
---local ltjs_orig_char_table = ltjs.orig_char_table
+local ltjs_orig_char_table = ltjs.orig_char_table
 local ltjf_get_vert_glyph = ltjf.get_vert_glyph
 local ltjf_replace_altfont = ltjf.replace_altfont
 local attr_orig_char = luatexbase.attributes['ltj@origchar']
@@ -67,9 +67,8 @@ do
       if (has_attr(p, attr_icflag) or 0)<=0 and is_ucs_in_japanese_char(p) then
          local pc = getchar(p)
 	 local pf = ltjf_replace_altfont(has_attr(p, attr_curjfnt) or getfont(p), pc)
-	 setfield(p, 'font', pf)
-	 setfield(p, 'lang', lang_ja)
-	 set_attr(p, attr_orig_char, pc)
+	 setfield(p, 'font', pf); setfield(p, 'lang', lang_ja)
+	 ltjs_orig_char_table[p] = pc
       end
       return p
    end
@@ -98,6 +97,9 @@ do
       local p = head
       for i = 1,#wt do wt[i]=nil end
       for i = 1,#wtd do wtd[i]=nil end
+      for i,_ in pairs(ltjs_orig_char_table) do
+	 ltjs_orig_char_table[i] = nil
+      end
       ltjs.list_dir=ltjd_get_dir_count()
       while p and p~=t do
 	 local pfunc = suppress_hyphenate_ja_aux[getid(p)]
@@ -130,13 +132,13 @@ local function set_box_stack_level(head, mode)
       for p in Dnode.traverse_id(id_glyph,to_direct(head)) do
          if (has_attr(p, attr_icflag) or 0)<=0 and getfield(p, 'lang')==lang_ja then
             local pfn = has_attr(p, attr_curtfnt) or getfont(p)
-            local pc = getchar(p)
+            local pc = ltjs_orig_char_table[p]
             local pf = ltjf_replace_altfont(pfn, pc)
-	    set_attr(p, attr_dir, pc)
-	    pc = ltjf_get_vert_glyph(pf, pc) or pc
-            setfield(p, 'char', pc); set_attr(p, attr_orig_char, pc)
-            setfield(p, 'font', pf); --set_attr(p, attr_curjfnt, pf)
-         end
+	    ltjs_orig_char_table[p] = { pc, ltjs_orig_char_table[p] }
+	    local xc = ltjf_get_vert_glyph(pf, pc) or pc
+            setfield(p, 'char', xc); setfield(p, 'font', pf);
+	    ltjs_orig_char_table[p] = { pc, xc }
+	 end
       end
    end
    --luatexja.ext_show_node_list(head, 'S> ', print)

src/ltj-stack.lua
@@ -111,6 +111,10 @@ function set_stack_skip(m,sp)
 end
 
 -- These three functions are used in ltj-jfmglue.lua.
+-- list_dir and orig_char_table are used in other lua files.
+orig_char_table = {}
+list_dir = nil -- dummy
+table_current_stack = nil -- dummy
 function report_stack_level(bsl)
    table_current_stack = charprop_stack_table[bsl]
    return bsl
@@ -129,12 +133,6 @@ function get_stack_table(mc, d, idx)
    local i = charprop_stack_table[idx][mc]
    return i or d
 end
-
---------------------------------------------------------------------------------
--- nest table (obeys TeX's semantic nest)
---------------------------------------------------------------------------------
-nest_table = {}
-local nest_table = nest_table
 
 
 -- EOF

commit 60378cef325e483158392b0e6e5adc1b50d5bd91

Fix a bug caused by commit 0526b27.

src/ltj-jfmglue.lua
@@ -112,7 +112,7 @@ do
    local stop_time_measure = ltjb.stop_time_measure
    slow_find_char_class = function (c, m, oc)
       local cls = ltjf_find_char_class(oc, m)
-      if oc~=c and cls==0 then
+      if c and oc~=c and cls==0 then
 	 return ltjf_find_char_class(-c, m), oc
       else
 	 return cls, oc
@@ -320,7 +320,7 @@ local min, max = math.min, math.max
 local function calc_np_aux_glyph_common(lp)
    Np.nuc = lp
    Np.id = npi
-   if ltjs_orig_char_table[lp] then
+   if getfield(lp, 'lang') == lang_ja then
       Np.id = id_jglyph
       set_np_xspc_jachar(Np, lp)
       local npi, npf
@@ -345,7 +345,7 @@ local function calc_np_aux_glyph_common(lp)
 	    lp=lx; break
 	 else
 	    local lid = getid(lx)
-	    if lid==id_glyph and not ltjs_orig_char_table[lx] then
+	    if lid==id_glyph and getfield(lx, 'lang') ~= lang_ja then
 	       -- 欧文文字
 	       last_glyph = lx; set_attr(lx, attr_icflag, PROCESSED); Np.last = lx
 	       y_adjust = has_attr(lx,attr_ablshift) or 0