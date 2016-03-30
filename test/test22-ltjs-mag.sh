dd366948cd8acadad123bd93751b623e9be91191
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2016-03-31 07:03:35 +0900

    updated test22-ltjs-mag.{tex,sh} (check 'xreal' option in ltjsclasses)

---
+++ test/test22-ltjs-mag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for x in `grep '{\\\\def\\\\ltjs@magscale' ../../src*/ltjsarticle.cls | grep 'DeclareOption' | sed 's/\\\\DeclareOption{\\([0-9][0-9.]*[A-Za-z]*\\)}.*/\\1/'`
+  do echo TEST: $x; luajitlatex "\\def\\fsize{$x}\\input test22-ltjs-mag" \
+  | grep @ | sed 's/@//'
+done
