d0dc2e59c10e519ce5c75e1c2c5ce695580a9dda
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2018-04-11 21:31:20 +0900

    Fix #38185

--- src/ltj-direction.lua
+++ src/ltj-direction.lua
@@ -640,7 +640,10 @@ do
       local bh = getlist(b)
       local box_dir, dn =  get_box_dir(b, ltjs.list_dir)
       -- 既に b の中身にあるwhatsit
-
+      if not dn then
+	bh = create_dir_whatsit(bh, 'make_dir_whatsit', dir_yoko)
+	dn = bh; setfield(b, 'head', bh)
+      end
       if box_dir%dir_math_mod==new_dir then
 	 if box_dir>=dir_node_auto then
 	    -- dir_node としてカプセル化されている
