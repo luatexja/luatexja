a5dd6c1d7e352515b8e578429b7765503db0e05f
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2019-03-29 16:09:16 +0900

    test22-ltjs-mag: adapt to latest ltjsclasses (and LaTeX)

--- test/test22-ltjs-mag.sh
+++ test/test22-ltjs-mag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for x in `grep '{\\\\def\\\\js@magscale' ../src*/ltjsarticle.cls | grep 'DeclareOption' | sed 's/\\\\DeclareOption{\\([0-9][0-9.]*[A-Za-z]*\\)}.*/\\1/'`
+for x in `grep '{\\\\def\\\\jsc@magscale' ../src*/ltjsarticle.cls | grep 'DeclareOption' | sed 's/\\\\DeclareOption{\\([0-9][0-9.]*[A-Za-z]*\\)}.*/\\1/'`
   do echo TEST: $x; luajitlatex "\\def\\fsize{$x}\\input test22-ltjs-mag" \
   | grep @ | sed 's/@//'
 done
